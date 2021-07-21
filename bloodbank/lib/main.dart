dd2f61ae8f726d5c02f1eff2ba51d61dad7f95bd
someshwari-rudra <somyarudra9@gmail.com>
2021-07-21 19:07:11 +0530

    flutter day4 completed

--- bloodbank/lib/main.dart
+++ bloodbank/lib/main.dart
@@ -1,6 +1,7 @@
 // import 'package:bloodbank/pages/home_page.dart';
 import 'package:bloodbank/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       home: loginPage(),
     );
   }
