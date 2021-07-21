2d09775aab5c5f27c59c0a36d74d1a727786ae76
someshwari-rudra <somyarudra9@gmail.com>
2021-07-21 12:09:40 +0530

    second commit

---
+++ bloodbank/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bloodbank/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      home: homePage(),
+    );
+  }
+}
---
+++ bloodbank/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Catolog App"),
+            ),
+        body: Center(
+          child: Container(
+            child: Text("welcome to 30 days of flutter"),
+          ),
+        ),
+    );
+  }
+}
